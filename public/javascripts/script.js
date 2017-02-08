f09326293778a266a40e42f6f1c1f236f155efbc
fashioncelle <jocelle.tanquion@gmail.com>
2017-02-08 11:29:47 +0800

    Sort Date Oldest to Newest

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,17 @@
 if (window.location.pathname === '/tedtalkslist') {
+	fetch('api/v1/tedtalks?sort=created').then(function(res) {
+			res.json().then(function(tedtalks) {
+				console.log('tedtalks', tedtalks);
+				tedtalks.forEach(function(tedtalks){
+				var tbody = document.getElementById('entry');
+					tbody.insertAdjacentHTML('beforeend', '<tr><td>' + '<a href="/list/' + tedtalks._id + '", class="red-text", style="font-size: 15px">' + tedtalks.title + '</td><td>' 
+					+ '<a class="grey-text", style="font-size: 15px">' + tedtalks.uploader_name + '</td><td>' 
+					+ '<a class="grey-text", style="font-size: 15px">' + tedtalks.views + '</td><td>' 
+					+ '<a class="grey-text", style="font-size: 15px">' + tedtalks.created + '</td></tr>');
+				});
+	      		});
+	    	});
+
 	fetch('api/v1/tedtalks/count').then(function(res){
 			res.json().then(function(count){
 				console.log('count', count)
@@ -7,3 +20,5 @@ if (window.location.pathname === '/tedtalkslist') {
 			});
 		});
 }
+
+
